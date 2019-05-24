c38c5879a36550f644b575f3a6adea6eafca020f
LuanaMora <luanamora32@gmail.com>
2019-05-24 10:50:11 -0300

    Tela de cadastro de imovel

--- app/src/main/java/com/android/projetoimobiliaria/ImovelActivity.java
+++ app/src/main/java/com/android/projetoimobiliaria/ImovelActivity.java
@@ -6,8 +6,17 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class ImovelActivity extends AppCompatActivity {
+
+    private TextView tvLabelImovel;
+    private EditText etCodigo, etDescricao, etTamanho;
+    private Spinner spCorretor, spLocatario;
+    private Button btEndereco, btSalvar, btCancelar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,15 +24,23 @@ public class ImovelActivity extends AppCompatActivity {
         setContentView(R.layout.activity_imovel);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        spCorretor.setPrompt("Corretor");
+        spLocatario.setPrompt("Locatario");
+    }
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    private void loadComponents(){
+        etCodigo = findViewById(R.id.etCodigo);
+        etDescricao = findViewById(R.id.etDescricao);
+        etTamanho = findViewById(R.id.etTamanho);
+        spCorretor = findViewById(R.id.spCorretor);
+        spLocatario = findViewById(R.id.spLocatario);
+        btEndereco = findViewById(R.id.btEndereco);
+        btSalvar = findViewById(R.id.btSalvar);
+        btCancelar = findViewById(R.id.btCancelar);
+    }
+
+    private void loadSpinner(){
+
     }
 
 }
