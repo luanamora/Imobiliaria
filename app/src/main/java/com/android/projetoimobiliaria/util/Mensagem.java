c52ffa99b57403a34363c82544b951be5a7b5e80
Eduardo Zils <eduardo.m.zils@gmail.com>
2019-05-24 16:14:28 -0300

    Icones icones de Mensagem; icones de menu lateral;
    Criado Mensagens de ALERTA, ERRO e SUCESSO;

---
+++ app/src/main/java/com/android/projetoimobiliaria/util/Mensagem.java
@@ -0,0 +1,27 @@
+package com.android.projetoimobiliaria.util;
+
+import android.app.AlertDialog;
+import android.content.Context;
+
+import com.android.projetoimobiliaria.R;
+
+public class Mensagem {
+    public static void ExibirMensagem(Context context, String msg, TipoMensagem tipo) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(context);
+
+        if (tipo == TipoMensagem.ALERTA) {
+            alert.setTitle("Atenção");
+            alert.setIcon(R.drawable.ic_alert);
+        } else if (tipo == TipoMensagem.ERRO) {
+            alert.setTitle("Erro");
+            alert.setIcon(R.drawable.ic_erro);
+        } else if (tipo == TipoMensagem.SUCESSO) {
+            alert.setTitle("Sucesso");
+            alert.setIcon(R.drawable.ic_check);
+        }
+        alert.setMessage(msg);
+        alert.setNeutralButton("Ok", null);
+        alert.show();
+
+    }
+}
